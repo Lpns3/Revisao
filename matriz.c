ffd680132318b6fc7cc6ac03854fa0606fdc1980
Luis Paulo Nery <l.pns@hotmail.com>
2016-08-26 22:18:04 -0300

    atributos adicionados

--- matriz.c
+++ matriz.c
@@ -18,13 +18,46 @@ int main(int argc, char *argv[])
 		for (k=0;k<4;k++)
 		{
 			//insere um valor em determinada posiçao
-			matriz[j][k]=rand ()%50;
+			matriz[j][k]=k;
 			//imprime a matriz
 			printf ("%i \t", matriz [j][k]);
 		}
 		//quebra a linha da matriz a partir da 4 coluna
-		printf ("\n\n");
+		printf ("\n");
 	}
+	//separa impreção da matriz completa da matriz identidade
+	printf ("\n");
+	//impreme o nome da matriz
+	printf ("Matriz Identidade: \n\n");
+	//lopp que varre a linha
+	for (j=0; j<4; j++) 
+	{
+		//loop que varre a coluna
+		for (k=0;k<4;k++)
+		{
+			//comparação para imprimir somente a identidade
+			if (j==k)printf ("%i \t", matriz [j][k]);
+		}
+	}
+	//separação da matriz identida da matriz sem identidade
+	printf ("\n\n");
+	//imprime o nome da matriz
+	printf ("Matriz sem Identidade: \n\n");
+	//loop varre linha
+	for (j=0; j<4; j++) 
+	{
+		//loop varre coluna
+		for (k=0;k<4;k++)
+		{
+			//imprime caso a coluna seja igual a linha
+			if (j==k)printf ("- \t");
+			//imprime caso a linha seja diferente da coluna
+			else printf ("%i \t", matriz [j][k]);
+		}
+		//quebra a linha
+		printf ("\n");
+	}
+	
 	//retorna zero
 	return 0;
 }
